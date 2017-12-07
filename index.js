f674a54c85b8c039cea7826f50d79fccaa2dbb7c
dancastillo <dancastillo99@gmail.com>
2017-12-07 12:46:47 -0500

    index.js: added jade and socket io to project

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
 app.get('/', (req,res) => {
-  res.send('it works')
+  res.render('page')
 });
 
-app.listen(port);
+app.use(express.static(__dirname + '/public'));
+
+// app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { message: 'welcome to the chat' });
+  socket.on('send', function (data) {
+      io.sockets.emit('message', data);
+  });
+});
 
 console.log(`Listening on port: ${port}`);
